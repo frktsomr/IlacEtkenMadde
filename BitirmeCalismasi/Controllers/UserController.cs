6760ac6f60f4b796e516d85f8f6ac60f5297cd5a
frktsomr <omer.tas@ogr.sakarya.edu.tr>
2022-05-05 05:09:20 +0300

    User işlemlerine başlanıldı

---
+++ BitirmeCalismasi/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitirmeCalismasi.Controllers
+{
+    public class UserController : Controller
+    {
+        UserManager um = new UserManager(new EfUserDal());
+        public ActionResult Index()
+        {
+            var UserValues = um.GetList();
+            return View(UserValues);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userdal;
+
+        public UserManager(IUserDal userdal)
+        {
+            _userdal = userdal;
+        }
+
+        public User GetByID(int id)
+        {
+            return _userdal.Get(x => x.UserID == id);
+        }
+
+        public List<User> GetList()
+        {
+            return _userdal.List();
+        }
+
+        public void UserAdd(User user)
+        {
+            _userdal.Insert(user);
+        }
+
+        public void UserDelete(User user)
+        {
+            _userdal.Delete(user); 
+        }
+
+        public void UserUptade(User user)
+        {
+            _userdal.Update(user);
+        }
+    }
+}
