c5623434e988e868240d5d9b5b6244f4c673e8f2
frktsomr <omer.tas@ogr.sakarya.edu.tr>
2022-05-06 04:49:48 +0300

    User Controller tamamlandı

--- BitirmeCalismasi/Controllers/UserController.cs
+++ BitirmeCalismasi/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,32 @@ namespace BitirmeCalismasi.Controllers
         {
             var UserValues = um.GetList();
             return View(UserValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(User user )
+        {
+            UserValidator uservalidator = new UserValidator();
+            ValidationResult result = uservalidator.Validate(user);
+            if(result.IsValid)
+            {
+                um.UserAdd(user);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kulanıcı Adı Boş Olamaz");
+            RuleFor(x => x.UserSurname).NotEmpty().WithMessage("Kulanıcı Soyadı Boş Olamaz");
+        }
+
+    }
+}
